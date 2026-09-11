7632a79973b2fea009c08ac60f99c3e97499b51c
dsboi-ouio <297689183+dsboi-ouio@users.noreply.github.com>
2026-09-12 00:06:45 +0800

    编写CMakeLists.txt

---
+++ build/CMakeFiles/cmake_basiclearn_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_basiclearn_1.dir/src/cmake_basiclearn.cpp.o"
+  "CMakeFiles/cmake_basiclearn_1.dir/src/cmake_basiclearn.cpp.o.d"
+  "CMakeFiles/cmake_basiclearn_1.dir/tools/hello.cpp.o"
+  "CMakeFiles/cmake_basiclearn_1.dir/tools/hello.cpp.o.d"
+  "cmake_basiclearn_1"
+  "cmake_basiclearn_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_basiclearn_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
